2f3c9b7eec30dd228c68859d4242d0747fbd2af3
Mclovin0213 <javellaneda0213@gmail.com>
2023-03-07 00:08:08 -0800

    Add VideoCards and ChannelCards to Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,20 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from '@mui/material'
 
-import Sidebar from "./Sidebar";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items));
+  }, [selectedCategory]);
+
+
   return (
     // Our feed will render in a column view, but when on
     // medium devices (md) will be in row view
@@ -13,7 +24,10 @@ const Feed = () => {
         borderRight: '1px solid #3d3d3d',
         px: { sx: 0, md: 2 }
       }}>
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography 
           className="copyright"
           variant="body2"
@@ -21,6 +35,27 @@ const Feed = () => {
         >
           Julian Avellaneda
         </Typography>
+      </Box>
+      <Box
+        p={2}
+        sx={{
+          overflowY: 'auto',
+          height: '90h',
+          flex: 2
+        }}
+
+      >
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: 'white'
+          }}
+        >
+          {selectedCategory} <span style={{ color : '#F31503' }}>videos</span>
+        </Typography>
+        <Videos videos={[videos]} />
       </Box>
     </Stack>
   )
