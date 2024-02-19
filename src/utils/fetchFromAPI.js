f2c13fe9f1a76311ef18eb09809883ebc31d84d0
Mclovin0213 <javellaneda0213@gmail.com>
2024-02-18 23:20:04 -0800

    switch api's

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,10 +4,10 @@ const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
     params: {
-      maxResults: '50'
+      maxResults: '10'
     },
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY ,
+      'X-RapidAPI-Key': process.env.API_KEY ,
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
